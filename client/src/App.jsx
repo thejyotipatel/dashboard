9651b4423d1faa7f641ad299460bbf465497d937
thejyotipatel <jyotipatelfyit77100@gmail.com>
2023-01-03 18:50:12 +0530

    styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,24 @@ import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
- 
+
   const getData = async () => {
     console.log('get data :---')
-    try{
+    try {
       let url = '/api/api/v1/data'
-      const {data} = await axios.get(url)
+      const { data } = await axios.get(url)
       console.log(data)
-
-    }catch(err){
-       console.log(err)
+    } catch (err) {
+      console.log(err)
     }
   }
-  
+
   return (
     <div className='App'>
       Click on the Vite and React logos to learn more
-      <button onClick={() => getData()}>Get data</button>
+      <button className='btn' onClick={() => getData()}>
+        Get data
+      </button>
     </div>
   )
 }
